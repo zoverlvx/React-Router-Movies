6827b40e6828a60377592badadc6e8fbd5790528
Zach Overhulser <zoverhulser@gmail.com>
2019-04-08 23:02:31 +0000

    Refactors movie card into separate component.

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import {Link} from "react-router-dom";
 import axios from 'axios';
 
+import MovieCard from "./MovieCard";
+
 export default class MovieList extends Component {
   constructor(props) {
     super(props);
@@ -33,25 +35,15 @@ export default class MovieList extends Component {
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars, id } = movie;
+  const { id } = movie;
   return (
-    	<div className="movie-card">
-			<Link to={`/movies/${id}`}>
-      			<h2>{title}</h2>
+			<Link 
+				style={{
+					color: "inherit",
+					textDecoration: "none"}} 
+				to={`/movies/${id}`}
+			>
+				<MovieCard movie={movie} />
 			</Link>
-      		<div className="movie-director">
-        		Director: <em>{director}</em>
-      		</div>
-      		<div className="movie-metascore">
-        		Metascore: <strong>{metascore}</strong>
-      		</div>
-      		<h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-	    </div>
   );
 }
