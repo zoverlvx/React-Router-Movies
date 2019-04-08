8c57b9dffd4b11ff2f4940f93a26f752d6c8a052
Zach Overhulser <zoverhulser@gmail.com>
2019-04-08 21:57:20 +0000

    Uses environment port to serve json data.

--- server.js
+++ server.js
@@ -66,6 +66,6 @@ app.post('/api/movies', (req, res) => {
 	res.status(201).json(movies);
 });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT, () => {
 	console.log('Server listening on port 5000');
 });
