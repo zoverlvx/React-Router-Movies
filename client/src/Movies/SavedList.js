696d46e9fceb07a4055a65a1e0db24df6628bd45
Zach Overhulser <zoverhulser@gmail.com>
2019-04-10 02:26:41 +0000

    Links back to movie card view.

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Link} from "react-router-dom";
+import {NavLink, Link} from "react-router-dom";
 
 export default class SavedList extends Component {
   constructor(props) {
@@ -11,7 +11,13 @@ export default class SavedList extends Component {
       <div className="saved-list">
         <h3>Saved Movies:</h3>
         {this.props.list.map(movie => (
-          <span className="saved-movie">{movie.title}</span>
+			<NavLink
+				to={`/movies/${movie.id}`}
+				key={movie.id}
+				activeClassName="saved-active"
+			>
+				<span className="saved-movie">{movie.title}</span>
+			</NavLink>
         ))}
         <Link to="/">
 			<div className="home-button">Home</div>
