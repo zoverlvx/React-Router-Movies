523e27db59c5c2e63ee469f4aa7a9c786a1bdf91
Zach Overhulser <zoverhulser@gmail.com>
2019-04-10 02:24:23 +0000

    Passes props down to Movie on specific route.

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,11 @@ export default class App extends Component {
       <div>
         <SavedList list={this.state.savedList} />
 		<Route exact path="/" component={MovieList} />
-		<Route path="/movies/:id" component={Movie} />
+		<Route path="/movies/:id" render={(props) => {
+			return (
+				<Movie {...props} addToSavedList={this.addToSavedList}/>
+			)
+		}} />
       </div>
     );
   }
